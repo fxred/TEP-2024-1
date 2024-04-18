cb6aff687b77e9bea21c2eb041501f0c763b4c73
fxred <fxr.ed03@gmail.com>
2024-04-18 10:52:04 -0300

    WA no teste 2 traveling salesman

--- L1/traveling_salesman_WA.cpp
+++ L1/traveling_salesman_WA.cpp
@@ -13,34 +13,38 @@ int main() {
     int cases;
     cin >> cases;
     for (int i = 0; i < cases; i++) {
-        int coordinates;
-        cin >> coordinates;
+        int number_of_pairs;
+        cin >> number_of_pairs;
         vector<Pair> coords;
-        for (int j = 0; j < coordinates; j++) {
+        for (int j = 0; j < number_of_pairs; j++) {
             Pair xy;
             cin >> xy.x;
             cin >> xy.y;
             coords.push_back(xy);
         }
-        Pair candidate;
-        candidate.x = 0, candidate.y = 0;
-        int min_dist = abs(candidate.x - coords[0].x) + abs(candidate.y - coords[0].y);
-        int dist_to_be_traveled = 0;
-        for (int m = 0; m < coords.size(); m++) {
+        Pair candidate = {0, 0};
+        int min_dist = 2147483647;
+        unsigned long long dist_to_be_traveled = 0;
+        while (coords.size() > 0) {
             int vector_index_to_be_erased = -1;
             for (int l = 0; l < coords.size(); l++) {
                 int diffX = candidate.x - coords[l].x;
                 int diffY = candidate.y - coords[l].y;
-                if (abs(diffX + diffY) <= min_dist) {
-                    min_dist = abs(diffX + diffY);
-                    candidate.x = coords[l].x;
-                    candidate.y = coords[l].y;
+                if (abs(diffX) + abs(diffY) < min_dist) {
+                    min_dist = abs(diffX) + abs(diffY);
                     vector_index_to_be_erased = l;
                 }
             }
+            candidate.x = coords[vector_index_to_be_erased].x;
+            candidate.y = coords[vector_index_to_be_erased].y;
             dist_to_be_traveled += min_dist;
+            min_dist = 2147483647;
             coords.erase(coords.begin() + vector_index_to_be_erased);
         }
-        
+        auto a = abs(candidate.x - 0);
+        auto b = abs(candidate.y - 0);
+        auto last_distance = a+b;
+        dist_to_be_traveled += last_distance;
+        cout << dist_to_be_traveled << endl;
     }
 }
